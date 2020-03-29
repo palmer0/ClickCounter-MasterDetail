86b8b6fb965210df87c8e40e15c1de00d6767b6c
Luis Hernandez <lhdez@iuma.ulpgc.es>
2020-03-29 16:19:32 +0100

    cambios menores

--- app/src/test/java/es/ulpgc/eite/cleancode/clickcounter/features/MasterUnitTests.java
+++ app/src/test/java/es/ulpgc/eite/cleancode/clickcounter/features/MasterUnitTests.java
@@ -4,79 +4,79 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class MasterSteps {
+public class MasterUnitTests {
 
 
   @Given("iniciamos pantalla maestro")
   public void iniciamos_pantalla_maestro() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Given("mostramos lista con {string} contadores")
   public void mostramos_lista_con_contadores(String string) {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @When("pulsamos boton lista")
   public void pulsamos_boton_lista() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Then("incluimos nuevo contador en posicion {string} de lista")
   public void incluimos_nuevo_contador_en_posicion_de_lista(String string) {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Then("mostramos contador con valor {string}")
   public void mostramos_contador_con_valor(String string) {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @When("pulsamos boton contador en {string}")
   public void pulsamos_boton_contador_en(String string) {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Then("iniciamos pantalla detalle")
   public void iniciamos_pantalla_detalle() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Then("mostramos numero de clicks con valor {string}")
   public void mostramos_numero_de_clicks_con_valor(String string) {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
 
   @Given("pulsamos boton contador")
   public void pulsamos_boton_contador() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @When("pulsamos boton regresar")
   public void pulsamos_boton_regresar() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Then("finalizamos pantalla detalle")
   public void finalizamos_pantalla_detalle() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 
   @Then("resumimos pantalla maestro")
   public void resumimos_pantalla_maestro() {
-    // Write code here that turns the phrase above into concrete actions
+    
     throw new cucumber.api.PendingException();
   }
 }
